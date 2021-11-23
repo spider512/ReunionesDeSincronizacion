efff2d3b1ca9c8ccb12d9e7fb1fc64f988366960
AlmironLeandro <leandroealmiron@hotmail.com>
2021-11-23 14:59:44 -0300

    implementado rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
+
+//Componentes propios
 import { ReunionDiariaComponent } from './reunion-diaria/reunion-diaria.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { PantallaQueVoyHacerHoyComponent } from './page-reuniones-diarias/pantalla-que-voy-hacer-hoy/pantalla-que-voy-hacer-hoy.component';
 
 const routes: Routes = [
-  { path: "", component: InicioComponent },
+  { path: "", component: InicioComponent, pathMatch: 'full' },
   { path: "reunion-diaria", component: ReunionDiariaComponent },
-  { path: "hoy", component: PantallaQueVoyHacerHoyComponent }
+  { path: "hoy", component: PantallaQueVoyHacerHoyComponent },
+  { path: "**", component: InicioComponent }
 ];
 
 @NgModule({
