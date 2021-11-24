192e9502a5270bf99466e5bd2af2b655152c7fdd
CasellaLautaro <lautaromcasella@gmail.com>
2021-11-24 02:10:30 -0300

    Uso de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { PantallaQueHiceAyerComponent } from './page-reuniones-diarias/pantalla-que-hice-ayer/pantalla-que-hice-ayer.component';
 import { ReunionDiariaComponent } from './reunion-diaria/reunion-diaria.component';
 
 const routes: Routes = [
   { path: "", component: InicioComponent },
-  { path: "reunion-diaria", component: ReunionDiariaComponent }
+  { path: "reunion-diaria", component: ReunionDiariaComponent },
+  { path: "ayer", component: PantallaQueHiceAyerComponent }
 ];
 
 @NgModule({
