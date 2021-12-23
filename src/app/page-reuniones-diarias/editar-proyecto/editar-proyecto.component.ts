ffeb2205775237860e5304f0fd2516a59c698a5f
CasellaLautaro <lautaromcasella@gmail.com>
2021-12-23 12:53:57 -0300

    Falta acomodar sub-nav y darle funcionalidad a eliminar en menu 'more vert'

--- src/app/page-reuniones-diarias/editar-proyecto/editar-proyecto.component.ts
+++ src/app/page-reuniones-diarias/editar-proyecto/editar-proyecto.component.ts
@@ -4,45 +4,45 @@ import { IoService } from 'src/app/shared/io.service';
 import { IProyecto } from '../../shared/clases'
 
 @Component({
-  selector: 'app-editar-proyecto',
-  templateUrl: './editar-proyecto.component.html',
-  styleUrls: ['./editar-proyecto.component.css']
+	selector: 'app-editar-proyecto',
+	templateUrl: './editar-proyecto.component.html',
+	styleUrls: ['./editar-proyecto.component.css']
 })
 export class EditarProyectoComponent implements OnInit {
-  public proyectoSeleccionado :any ;
-  public oProyecto:IProyecto={};
-public estados:any[]=[{estado:1, descripcion:"No iniciado"},{estado:2, descripcion:"Iniciado"},{estado:3, descripcion:"Finalizado"},{estado:4, descripcion:"Cancelado"}];
+	public proyectoSeleccionado: any;
+	public oProyecto: IProyecto = {};
+	public estados: any[] = [{ estado: 1, descripcion: "No iniciado" }, { estado: 2, descripcion: "Iniciado" }, { estado: 3, descripcion: "Finalizado" }, { estado: 4, descripcion: "Cancelado" }];
 
 
-  constructor(public ruta:Router, router:ActivatedRoute, public io: IoService) {
-    
-    this.proyectoSeleccionado = router.snapshot.paramMap.get("id");
-    if (this.proyectoSeleccionado) {
-      this.cargarProyecto(this.proyectoSeleccionado);
-    }
+	constructor(public ruta: Router, router: ActivatedRoute, public io: IoService) {
 
-  }
+		this.proyectoSeleccionado = router.snapshot.paramMap.get("id");
+		if (this.proyectoSeleccionado) {
+			this.cargarProyecto(this.proyectoSeleccionado);
+		}
 
-async cargarProyecto(id:number){
+	}
 
-  let proyectos:IProyecto[]=await this.io.proyectos(id);
-  this.oProyecto= proyectos[0];
-}
+	async cargarProyecto(id: number) {
 
-  ngOnInit(): void {
-  }
+		let proyectos: IProyecto[] = await this.io.proyectos(id);
+		this.oProyecto = proyectos[0];
+	}
 
-  volver(){
-    this.ruta.navigate(['/seleccionar-proyecto']);
+	ngOnInit(): void {
+	}
 
-  }
+	volver() {
+		this.ruta.navigate(['/seleccionar-proyecto']);
 
-grabar(){
-console.log (JSON.stringify(this.oProyecto));
-this.io.grabarProyecto(this.oProyecto).then(()=>{
-  this.volver();
-});
+	}
 
-}
+	grabar() {
+		console.log(JSON.stringify(this.oProyecto));
+		this.io.grabarProyecto(this.oProyecto).then(() => {
+			this.volver();
+		});
+
+	}
 
 }
